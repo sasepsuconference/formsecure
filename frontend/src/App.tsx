ce6c43c348e6a02a007855b1dec434988f907b44
MatthewPun06 <matthewpun06@gmail.com>
2026-07-26 02:29:28 -0400

    cleanup: remove unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,11 @@ import { useEffect, useState } from 'react'
 import Login from './components/Login.tsx'
 import MenuBar from './components/MenuBar.tsx'
 import Dashboard from './components/Dashboard.tsx'
-import Form from './components/Form.tsx'
 import './index.css'
 import { getCurrentUser } from './services/user.tsx';
-import FormEditor from './components/FormEditor.tsx'
 
 function App() {
   const [dark, setDark] = useState(false)
-  const [page, setPage] = useState("dashboard");
   const [signedIn, setSignIn] = useState(false)
   // Declare the inner async function
   useEffect(() => {
@@ -36,16 +33,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen bg-bg text-text transition-colors duration-200">
       <MenuBar dark={dark} onToggleDark={() => setDark((prev) => !prev)} setSignedIn={setSignIn} />
-      {signedIn ? (() => {
-        switch (page) {
-          case 'edit':
-            return <p>Data loaded successfully!</p>;
-          case 'respond':
-            return <p>An error occurred.</p>;
-          default:
-            return <Dashboard/>;
-        }
-      })() : <Login setSignedIn={setSignIn} />
+      {signedIn ? <Dashboard/> : <Login setSignedIn={setSignIn} />
       }
     </div>
   )
